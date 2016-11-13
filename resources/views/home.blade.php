5c397ce8eda901aed9cd1e07ffd7fc5d9a5936d1
Eligio Arreola <eligio_arreola@hotmail.com>
2016-11-13 01:18:27 -0600

    Error de logout, cambios tabla prueba en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,104 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<style >
-    
-   h2 { 
-    font-size: 36px;
-    margin: 0 0 1px 0;
-   }
-   p{
-    margin: 0 0 10px 0
-   }
-   table.ancho,table.rwd_auto{
-    border: 1px solid #ccc;
-    width: 100%;
-    margin: 0 0 50px 0;
-   }
-   .ancho th,.red_auto, th{
-    background:#ccc;
-    padding: 5px;
-    text-align: center; 
-   }
-   .ancho td,.red_auto td {
-    border-bottom: 1px solid #ccc;
-    padding: 5px;
-    text-align: center;
-   }
-   .ancho tr:last-child td, .red_auto tr:last-child td{border: 0;}
-   .rwd{
-    width: 100%;
-    overflow: auto;
-   }
-   .rwd table.rwd_auto{
-    white-space: nowrap;
-   }
-
-   @media only screen and (max-width: 760px), (min-width:768px) and (max-width: 1024px)
-   {
-     table.ancho, .ancho thead, .ancho tbody, .ancho td, .ancho tr{ display: block; }
-     .ancho thead tr {position: absolute;top: -9999px;}
-     .ancho tr{ border: 1px solid #ccc; }
-     .ancho td{border:none; border-bottom: 1px solid #ccc; position: relative;padding-left: 50%; text-align: left;}
-     .ancho td:before {position: absolute; top: 6px; left: 6px; width: 45%; padding-right: 10px; white-space: nowrap;}
-     .ancho td:nth-of-type(1):before{content: "#"}
-     .ancho td:nth-of-type(2):before{content: "First Name"}
-     .ancho td:nth-of-type(3):before{content: "Last Name"}
-     .ancho td:nth-of-type(4):before{content: "Username"}
-
-     .descarto {display: none;}
-     .fontsize {font-size: 10px;}
-   }
-
-   @media only screen and(min-width: 320px) and(max-width: 480px)
-   {
-    body {width: 320px;}
-    .descarto {display: none;}
-   }
-   @media only screen and(min-width: 768px) and(max-width: 1024px)
-   {
-    body {width: 495px}
-    .descarto {display: none;}
-    .fontsize {font-size: 10px;}
-   }
-
-
-</style>
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-<table class="ancho">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody >
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-  </table>
+
 
                 <div class="panel-body">
                     You are logged in!
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,10 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-    <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
+
     <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
+     <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen" />
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -82,7 +78,6 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
